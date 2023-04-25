4bc6beac7eec38fef8214caf710863f1dab99e3f
maxfahl <max@fahl.se>
2023-04-25 20:23:48 +0200

    Prompr improvements.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ MODEL = config('MODEL', default="gpt-3.5-turbo")
 if not OPENAI_API_KEY:
     print('OpenAI API key not defined in .env.')
     quit()
-openai.api_key = OPENAI_API_KEY
 
+openai.api_key = OPENAI_API_KEY
 
 # Available colors in termcolor, ignore gray and white.
 COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
@@ -46,19 +46,22 @@ def get_chat_gpt_response(prompt):
     return response.choices[0].message['content'].strip()
 
 
-def generate_message(person, conversation):
+def generate_message(person_to_answer, previous_conversation, people):
     prompt = [
-        {"role": "system", "content": f"You are {person.name}, {person.description}."},
+        {"role": "system", "content": f"You are {person_to_answer.name}, {person_to_answer.description}."},
     ]
 
-    if len(conversation) == 0:
-        prompt[0]['content'] += f" Come up with a short to medium sized text message that ends with question or a statement."
+    if len(previous_conversation) == 0:
+        prompt[0][
+            'content'] += f" Come up with a short to medium sized text message that ends with question or a statement."
     else:
-        prompt[0]['content'] += f" Come up with a short to medium sized text message that answers the previous message."
+        prompt[0]['content'] += f" Come up with a short to medium sized text message that answers the previous message. Keep track of the names in the beginning of each message to identify what person wrote what answer."
 
-    prompt[0]['content'] += f" Answer as if you know the other person well, do not be formal or apolagetic unless the description of yourself says that you are. keep track of the names in the beginning of each message to identify what person wrote what answer. Do not type the name of the person in the beginning of your response. "
+    roles = [f"{i + 1}. {person.name.capitalize()}, {person.description}" for i, person in enumerate(people)]
+    roles_str = "\n".join(roles)
+    prompt[0]['content'] += f" You know the people in the chat very well. Do not be formal or apolagetic unless the description of yourself says that you are. Only answer from one person at a time, that is yourself. Do not type the name of yourself or another person in the beginning of the message. Here's a list of the people involved:\n" + roles_str
 
-    for msg in conversation:
+    for msg in previous_conversation:
         role, content = msg
         prompt.append({"role": role, "content": content})
 
@@ -67,12 +70,18 @@ def generate_message(person, conversation):
 
 
 def get_best_fit_person_index(people, conversation, latest_writer_index):
-    roles = [{"role": "system", "content": f"{i+1}: {person.name}, {person.description}"} for i, person in enumerate(people)]
+    roles = [f"{i + 1}. {person.name.capitalize()}, {person.description}" for i, person in enumerate(people)]
+    roles_str = "\n".join(roles)
 
-    prompt = [
-                 {"role": "system", "content": "Choose the best person to respond to the latest message based on the content and the message history as a whole. Only respond with an integer representing the number of the person in the list (example response: 1). Here's the list of the people involved:\n"},
-             ] + roles
+    content = ''
+    if len(conversation) == 0:
+        content = "Choose the best person to start the conversation."
+    else:
+        content = "Choose the best person to respond to the latest message based on the content and the message history as a whole."
 
+    content += " Only respond with an integer representing the number of the person in the list (example response: 1). Here's the list of the people involved:\n" + roles_str
+
+    prompt = [{"role": "system", "content": content}]
     response = get_chat_gpt_response(prompt)
 
     try:
@@ -93,7 +102,7 @@ def chat_simulation(people, iterations):
 
     for i in range(iterations):
         best_fit_person_index = get_best_fit_person_index(people, conversation, latest_writer_index)
-        message = generate_message(people[best_fit_person_index], conversation)
+        message = generate_message(people[best_fit_person_index], conversation, people)
         conversation.append(("user", f"{people[best_fit_person_index].name}: {message}"))
         cprint(f"\n{people[best_fit_person_index].name}: {message}", people[best_fit_person_index].color)
 
@@ -102,7 +111,8 @@ def chat_simulation(people, iterations):
 
 def get_person_input(person_number):
     name = input(f"Enter the name for person {person_number}: ")
-    description = input(f"Enter a description for person {person_number} (example: 'a computer nerd who loves coffee'): ")
+    description = input(
+        f"Enter a description for person {person_number} (example: 'a computer nerd who loves coffee'): ")
     return name, description
 
 
@@ -117,7 +127,8 @@ def create_and_save_people(num_people):
 
 
 def save_to_json(people):
-    data = {f"person{i + 1}": {"name": person.name, "description": person.description} for i, person in enumerate(people)}
+    data = {f"person{i + 1}": {"name": person.name, "color": person.color, "description": person.description} for i, person in
+            enumerate(people)}
     with open("names_and_personalities.json", "w") as f:
         json.dump(data, f)
 
@@ -125,7 +136,8 @@ def save_to_json(people):
 def load_from_json():
     with open("names_and_personalities.json", "r") as f:
         data = json.load(f)
-    people = [create_person(person_data["name"], person_data["description"], person_data["color"]) for person_data in data.values()]
+    people = [create_person(person_data["name"], person_data["description"], person_data["color"]) for person_data in
+              data.values()]
     return people
 
 
