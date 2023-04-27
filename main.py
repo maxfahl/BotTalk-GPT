79875c856bf1e3f4c1f80962f0f3f4a74f5a3c7e
maxfahl <max@fahl.se>
2023-04-27 14:22:19 +0200

    Switched up the color order a bit to avoid red as much as possible.

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if ELEVENLABS_API_KEY:
     set_api_key(ELEVENLABS_API_KEY)
 
 # Available colors in termcolor, ignore gray and white.
-COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
+COLORS = ['yellow', 'green', 'blue', 'magenta', 'cyan', 'red']
 VOICES = {
     'f': ['Rachel', 'Domi', 'Bella', 'Elli'],
     'm': ['Antoni', 'Josh', 'Arnold', 'Adam', 'Sam']
